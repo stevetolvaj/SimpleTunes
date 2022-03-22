62ec6b3c84b0a932a0b458fe9ada7e5b290cc59e
Alan Freeman <alan.freeman@temple.edu>
2022-03-22 15:05:51 -0400

    new branch

--- app/src/main/java/edu/temple/simpletunes/MainActivity.java
+++ app/src/main/java/edu/temple/simpletunes/MainActivity.java
@@ -2,7 +2,10 @@ package edu.temple.simpletunes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        Button browserButton = findViewById(R.id.browserButton);
+        browserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        super.onResume();
     }
 }
