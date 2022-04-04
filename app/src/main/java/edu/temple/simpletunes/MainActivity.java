9c914ab77b80893af4ab2aafaecb7ca7b648b9c2
DarlinKola1 <89646919+DarlinKola1@users.noreply.github.com>
2022-04-04 10:59:10 -0400

    Added code to make music stop and start where it left off.

--- app/src/main/java/edu/temple/simpletunes/MainActivity.java
+++ app/src/main/java/edu/temple/simpletunes/MainActivity.java
@@ -9,52 +9,33 @@ import androidx.core.content.ContextCompat;
 import androidx.documentfile.provider.DocumentFile;
 
 import android.Manifest;
-import android.content.ComponentName;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.io.IOException;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
+    private static MediaPlayer player;
     private ActivityResultLauncher<Intent> mActivityResultLauncher;
     private ActivityResultLauncher<Intent> folderLauncher;
+    private static final int REQUEST_MP3 = 23;
     private static final int STORAGE_PERMISSION_CODE = 101;
-
-
-    // Variables and initialization of MediaPlayerService service connection.
-    // TODO: use functions available through mAudioControlsBinder to control media.
-    // mMediaControlsBinder.play, pause, resume, stop, isPlaying.
-    // resume and pause does not check if track is playing or already paused.
-    private boolean isConnected = false;
-    private MediaPlayerService.ControlsBinder mAudioControlsBinder;
-    private final ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            isConnected = true;
-            mAudioControlsBinder = (MediaPlayerService.ControlsBinder) service;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            isConnected = false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Bind the MediaPlayerService to the MainActivity.
-        Intent intent = new Intent(this, MediaPlayerService.class);
-        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
 
         mActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if(result.getResultCode() == RESULT_OK && result.getData() == null){
@@ -79,9 +60,12 @@ public class MainActivity extends AppCompatActivity {
                     if(directory == null){
                         Log.d(TAG, "onActivityResult: got empty directory");
                     }else{
-                        DocumentFile[] contents = directory.listFiles();
-                        Log.d(TAG, "onCreate: Folder passed to MediaPlayerService. Items in folder: " + contents.length);
-                        mediaPlayerPlayFolder(contents);
+                        DocumentFile contents[] = directory.listFiles();
+                        for(DocumentFile df : contents){
+                            Uri u = df.getUri();
+                            Log.d(TAG, "onActivityResult: sending URI to player: " + u.toString());
+                            mediaPlayerPlay(u);
+                        }
                     }
                 }
             }
@@ -139,35 +123,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         super.onResume();
+        ImageButton playPauseButton = findViewById(R.id.playPauseButton);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(player.isPlaying()){
+                    player.pause();
+                }else if(!player.isPlaying()){
+                    player.start();
+                }
+            }
+        });
     }
 
     /**
-     * The mediaPlayerPlay method is used to start the MediaPlayerService and
-     * also play the associated Uri.
+     * The mediaPlayerPlay method is used to initialize the mediaPlayer
+     * play the Uri provided. Also checks if reset is needed to play new audio file.
+     * Plays asynchronously
      * @param myUri The Uri to start playing.
      */
     private void mediaPlayerPlay(Uri myUri) {
-        if (isConnected) // Start service if first time playing a track.
-            startService(new Intent(this, MediaPlayerService.class));
-        mAudioControlsBinder.play(myUri);
-    }
 
-    /**
-     * The mediaPlayerPlayFolder plays the entire folder found in a DocumentFile array. Stops after
-     * last file is completed playing.
-     * @param folder The DocumentFile array to play all audio files from.
-     */
-    private void mediaPlayerPlayFolder(DocumentFile[] folder) {
-        if (isConnected)
-            startService(new Intent(this, MediaPlayerService.class));
-        mAudioControlsBinder.playFolder(folder);
+        if (player == null) {
+            player = new MediaPlayer();
+        } else {
+            player.reset();   // Reset to change data source.
+        }
+        player.setAudioAttributes(new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build());
+        try {
+            player.setDataSource(getApplicationContext(), myUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.prepareAsync();
+        player.setOnPreparedListener(MediaPlayer::start);
     }
 
     @Override
     protected void onDestroy() {
+        if (!isChangingConfigurations()) {
+            if (player != null) {
+                if (player.isPlaying()) {
+                    player.stop();
+                }
+                player.release();
+                player = null;
+            }
+        }
         super.onDestroy();
-        unbindService(mServiceConnection);
-        if (!isChangingConfigurations())
-            stopService(new Intent(this, MediaPlayerService.class));
     }
 }
